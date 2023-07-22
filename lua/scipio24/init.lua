9f3f26cb8b0dc46fa5ce151f216bfb424912e0d0
Andrei Pop <andrei.pop@hpm.ro>
2023-07-22 20:14:40 +0300

    Recreate the configs based on primeagen guide

---
+++ lua/scipio24/init.lua
@@ -0,0 +1,11 @@
+require('scipio24.remap')
+require('scipio24.set')
+
+require('onedark').setup {
+    style = 'darker'
+}
+require('onedark').load()
+
+require('lualine').setup {
+    options = { theme = 'auto' }
+}
