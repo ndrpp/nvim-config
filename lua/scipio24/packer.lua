959702b1c388ffe87adad6fe4ef1c67470c9df8d
Andrei Pop <andrei.pop@hpm.ro>
2023-07-30 16:21:28 +0300

    Add VimBeGood game

--- lua/scipio24/packer.lua
+++ lua/scipio24/packer.lua
@@ -50,4 +50,6 @@ return require('packer').startup(function(use)
   use 'nvim-tree/nvim-web-devicons'
 
   use {'nvim-lualine/lualine.nvim', requires = {'nvim-tree/nvim-web-devicons', opt=true}}
+
+  use 'ThePrimeagen/vim-be-good'
 end)
