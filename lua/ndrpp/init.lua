c8351c31804f12eba272f90eeb8b9657bf75c812
ndrpp <popandrei230@yahoo.com>
2025-09-19 16:33:02 +0300

    Add hard contrast to gruvbox theme

--- lua/ndrpp/init.lua
+++ lua/ndrpp/init.lua
@@ -5,6 +5,9 @@ require("ndrpp.lazy")
 --vim.cmd.colorscheme "catppuccin"
 --vim.cmd.colorscheme "rose-pine"
 --vim.cmd.colorscheme("kanagawa")
+require("gruvbox").setup({
+    contrast = "hard"
+})
 vim.cmd.colorscheme("gruvbox")
 
 require("lualine").setup({
